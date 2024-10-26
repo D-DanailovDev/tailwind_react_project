9ba20e5ae0f48a7ef68c5ab1ba232a76760285a7
D.Danailov <ddanailov.dev@gmail.com>
2024-10-26 19:12:45 +0300

    files/folders structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import Nav from "./components/Nav.jsx";
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections/index.js";
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">Popular Products</section>
-    <section className="padding">Super Quality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">Special Offers</section>
-    <section className="bg-pale-blue padding">Customer Reviews</section>
-    <section className="padding-x w-full sm:py-32 py-16">Subscribe</section>
-    <section className="padding-x padding-t pb-8 bg-black">Footer</section>
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x w-full sm:py-32 py-16">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      <Footer />
+    </section>
   </main>
 );
 
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div>This is the hero section</div>;
+};
+
+export default Hero;
