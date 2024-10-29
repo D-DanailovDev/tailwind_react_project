ff06956c464223c7dc2369b0df1ac56913c660b5
D.Danailov <ddanailov.dev@gmail.com>
2024-10-29 17:06:24 +0200

    quality section remake ready

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right"
-        className="ml-2 w-5 h-5 rounded-full"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right"
+          className="ml-2 w-5 h-5 rounded-full"
+        />
+      )}
     </button>
   );
 };
