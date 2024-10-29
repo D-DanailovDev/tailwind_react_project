ccb6c3a58789c95884892b8d0886f2ef5e793d1e
D.Danailov <ddanailov.dev@gmail.com>
2024-10-29 19:54:17 +0200

    Button component modified; Resize problem solved; Special Offer section - done

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,6 @@
 import { services } from "../constants";
 import ServiceCard from "../components/ServiceCard";
 
-console.log(services);
-
 const Services = () => {
   return (
     <section className="max-container flex justify-center flex-wrap gap-9">
