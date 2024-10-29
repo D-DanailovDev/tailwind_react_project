2792d24ccf905736e0747d5c752f351f68988618
D.Danailov <ddanailov.dev@gmail.com>
2024-10-29 19:15:03 +0200

    new service component added; section ready

--- src/constants/index.js
+++ src/constants/index.js
@@ -69,18 +69,18 @@ export const products = [
 
 export const services = [
   {
-    image: truckFast,
+    imgURL: truckFast,
     label: "Free Shipping",
     subtext: "Enjoy seamless shopping with our complimentary shipping service.",
   },
   {
-    image: shieldTick,
+    imgURL: shieldTick,
     label: "Secure Payment",
     subtext:
       "Experience worry-free transactions with our secure payment options.",
   },
   {
-    image: support,
+    imgURL: support,
     label: "Love to help you",
     subtext: "Our dedicated team is here to assist you every step of the way.",
   },
